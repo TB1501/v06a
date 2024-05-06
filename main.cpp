7888bf224a9058e3b214e497c6c078da4e7f230b
Tin Bašić <tin.basic22@gmail.com>
2024-05-06 18:05:41 +0200

    V06a TB

--- main.cpp
+++ main.cpp
@@ -6,23 +6,80 @@ int size_dialog::idd() const {
 }
 
 bool size_dialog::on_init_dialog(){
+	SetDlgItemInt(*this, IDC_EDIT1, BOARD_SIZE, false);
 	return true;
 }
 
 bool size_dialog::on_ok(){
+	newBoardSize = GetDlgItemInt(*this, IDC_EDIT1, NULL, false);
 	return true;
 }
 
 
 void main_window::on_paint(HDC hdc){
+	RECT rc;
+	GetClientRect(*this, &rc);
+	
+	int width = BOARD_SIZE * SQUARE_SIZE;
+	int height = BOARD_SIZE * SQUARE_SIZE;
+
+	int x = (rc.right - width) / 2;
+	int y = (rc.bottom - height) / 2;
+
+	HBRUSH whiteBrush = CreateSolidBrush(RGB(255, 255, 255));
+	HBRUSH blackBrush;
+	if (useCustomColors) {
+		blackBrush = CreateSolidBrush(squareColor1);
+	}
+	else {
+		blackBrush = CreateSolidBrush(RGB(0, 0, 0));
+	}
+	 
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			RECT square = {x + i * SQUARE_SIZE, y + j * SQUARE_SIZE, x + (i + 1) * SQUARE_SIZE, y + (j + 1) * SQUARE_SIZE};
+			HBRUSH brush = (i + j) % 2 == 0 ? whiteBrush : blackBrush;
+			FillRect(hdc, &square, brush);
+		}
+	}
+	DeleteObject(whiteBrush);
+	if (useCustomColors) {
+		DeleteObject(blackBrush);
+	}
 }
 
 void main_window::on_command(int id){
-	switch(id){
-		case ID_SIZE:
-			break;
-		case ID_COLOR:
-			break;
+	switch (id) {
+	case ID_SIZE: {
+				size_dialog dlg;
+				if (dlg.do_modal(0, *this) == IDOK)
+				{
+					BOARD_SIZE = dlg.newBoardSize;
+					InvalidateRect(*this, NULL, true);
+
+				}
+
+				break;
+	}
+	case ID_COLOR: {
+		
+		CHOOSECOLOR cc;
+		COLORREF customColors[16] = { 0 };
+		ZeroMemory(&cc, sizeof(cc));
+		cc.lStructSize = sizeof(cc);
+		cc.hwndOwner = *this;
+		cc.lpCustColors = customColors;
+		cc.rgbResult = RGB(255, 255, 255);
+		cc.Flags = CC_FULLOPEN | CC_RGBINIT;
+
+		if (ChooseColor(&cc) == TRUE) {
+			squareColor1 = cc.rgbResult;
+			squareColor2 = ~cc.rgbResult;
+			useCustomColors = true;
+			InvalidateRect(*this, 0, true);
+		}
+		break;
+	}
 		case ID_EXIT: 
 			DestroyWindow(*this); 
 			break;
--- main.h
+++ main.h
@@ -3,11 +3,16 @@
 #include "nwpwin.h"
 #include "nwpdlg.h"
 
+int BOARD_SIZE = 8;
+int SQUARE_SIZE = 50;
+
 class size_dialog : public vsite::nwp::dialog {
 protected:
 	int idd() const override;
 	bool on_init_dialog() override;
 	bool on_ok() override;
+public:
+	int newBoardSize;
 };
 
 class main_window : public vsite::nwp::window {
@@ -15,4 +20,10 @@ protected:
 	void on_paint(HDC hdc) override;
 	void on_command(int id) override;
 	void on_destroy() override;
+
+	COLORREF squareColor1;
+	COLORREF squareColor2;
+	bool useCustomColors;
+
+	void ChooseCustomColor();
 };
